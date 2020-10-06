8eaa6d269b13855983332dfc6af6a9694a7253cd
Justin Bender <jrkbender@gmail.com>
2020-10-05 23:19:29 -0400

    adding labelling

--- classes/person.class.php
+++ classes/person.class.php
@@ -1,33 +1,43 @@
 <?php
 
+// Creating a Person class with static values and new Person object values
 class Person {
     // Properties
+    // Private so cannot be accessed outside of the class
     private $name;
     private $age;
 
+    // constructing a new object with new Person
     public function __construct($name, $age) {
         $this->name = $name;
         $this->age = $age;
     }
 
+    // this is a static value to new Person does not need to be called.
+    // value obtained from Person::$homePlanet
     public static $homePlanet = "Earth";
 
+    // allows private age value to be accessed outside of class
     public function getAge() {
         echo $this->age;
     }
 
+    // allows private name value to be accessed outside of class
     public function getName() {
         echo $this->name;
     }
 
+    // allows private age value to be set
     public function setAge($age) {
         $this->name = $age;
     }
 
+    // allows private name value to be set
     public function setName($name) {
         $this->name = $name;
     }
 
+    // static function accessed by Person::setPlanetHome();
     public static function setPlanetHome($newPlanet) {
         self::$planet = $newPlanet;
     }
